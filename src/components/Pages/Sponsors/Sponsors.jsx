91ae5ec1236b5a9e0d3961d0041bed3c7361d736
kevin4809 <kevinedk33@gmail.com>
2023-01-23 14:02:09 -0500

    animation scroll
    animation scroll

--- src/components/Pages/Sponsors/Sponsors.jsx
+++ src/components/Pages/Sponsors/Sponsors.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import sponsors from "./Sponsors.json";
+import { scrollElement } from "../../helpers/ScrollAnimations";
 export const Sponsors = () => {
   const [infoSponsors, setInfoSponsors] = useState([]);
-
+  // const containerRef = useRef(null);
+  // const [isVisible, setIsVisible] = useState(false);
   useEffect(() => {
     setInfoSponsors(sponsors);
   }, []);
@@ -15,7 +17,44 @@ export const Sponsors = () => {
       body.style.overflowY = "hidden";
       body.style.overflowY = "scroll";
     }, 500);
+
+    // const cards = document.querySelector(".card");
+    // console.log(cards);
+    // // const observer = new IntersectionObserver(animActive, {
+    // //   root: null,
+    // //   rootMargin: "0px 0px 0px 0px ",
+    // //   threshold: 1.0,
+    // // });
+
+    // // observer.observe(cards);
   }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+      scrollElement("card", "anim_card_view");
+    });
+  }, []);
+
+  // const callbackFunction = (entries) => {
+  //   const [entry] = entries;
+  //   // setIsVisible(entry.isIntersecting);
+  //   const card = document.querySelector(".card");
+  //   card.classList.toggle("anim_card_view");
+  //   console.log(entry.isIntersecting);
+  // };
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(callbackFunction, {
+  //     root: null,
+  //     rootMargin: "0px",
+  //     threshold: 1.0,
+  //   });
+  //   if (containerRef.current) observer.observe(containerRef.current);
+  //   console.log("Esto es lo que me interesa " + containerRef.current);
+  //   return () => {
+  //     if (containerRef.current) observer.unobserve(containerRef.current);
+  //   };
+  // }, [containerRef, infoSponsors]);
 
   return (
     <div className="sponsors">
@@ -27,8 +66,8 @@ export const Sponsors = () => {
         <div className="content_info containerAll">
           <h1>SPONSORS</h1>
           <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+            eirmod tempor invidunt ut labore et dolore
           </p>
         </div>
 
@@ -40,11 +79,7 @@ export const Sponsors = () => {
             alt="Montaña"
           />
 
-          <img
-            className="flor"
-            src="/assets/etapa4/inicio/flor1.png"
-            alt="flor"
-          />
+          <img className="flor" src="/assets/etapa4/inicio/flor1.png" alt="flor" />
 
           <img className="hulk" src="/assets/etapa5/sponsors.png" alt="señor" />
         </div>
@@ -52,9 +87,11 @@ export const Sponsors = () => {
         <div className="content_cards">
           {infoSponsors.map((item, i) => (
             <div key={i} className="card">
-              <img src={item.logo} alt="logo" />
+              <img className="imgPrueba" src={item.logo} alt="logo" />
               <p>{item.info}</p>
-              <a href={item.link}>VER MÁS</a>
+              <a target="_blank" href={item.link}>
+                VER MÁS
+              </a>
             </div>
           ))}
         </div>
---
+++ src/components/helpers/ScrollAnimations.jsx
@@ -0,0 +1,25 @@
+export const middleScreen = (elemento, clase, heightTotal) => {
+  const contenido = document.querySelector(`.${elemento}`);
+
+  if (contenido !== null) {
+    const rect = contenido.getBoundingClientRect();
+
+    if (rect.top / 2 <= heightTotal / 2 / 2) {
+      contenido.classList.add(clase);
+    } else {
+      contenido.classList.remove(clase);
+    }
+  }
+};
+
+export const scrollElement = (itemsClass, classAnimation) => {
+  const heightTotal = window.innerHeight;
+  let items = document.querySelectorAll(`.${itemsClass}`);
+  if (items.length >= 2) {
+    for (let i = 0; items.length > i; i++) {
+      middleScreen(`${itemsClass}_${i + 1}`, classAnimation, heightTotal);
+    }
+  } else {
+    middleScreen(itemsClass, classAnimation, heightTotal);
+  }
+};
